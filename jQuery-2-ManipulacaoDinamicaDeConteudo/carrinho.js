38615b1717647ef4c9ce92d1d3dabf0c2bf22b88
Bruno Naibert <brunonaibert@hotmail.com>
2017-04-29 00:41:11 -0300

    Mais seletores

--- jQuery-2-ManipulacaoDinamicaDeConteudo/carrinho.js
+++ jQuery-2-ManipulacaoDinamicaDeConteudo/carrinho.js
@@ -1,3 +1,18 @@
+var umaPropaganda = function() {
+  var propagandas = ['O que acha de comprar uma lancha?',
+                      'O que acha de comprar uma motocicleta?',
+                      'O que acha de comprar uma bicicleta?',
+                      'O que acha de comprar um carro?',
+                    ]
+  var posicao = Math.floor(propagandas.length * Math.random());
+  var texto = propagandas[posicao];
+  var tr = $('<tr>').addClass('.propaganda').append($('<td colspan="6">'));
+  tr.find('td').text(texto);
+  return tr;
+};
+
+
+
 var atualizaDados = function() {
   var carrinhos = $('.carrinho');
   carrinhos.each(function() {
@@ -38,5 +53,8 @@ var aposInicializado = function() {
   atualizaDados();
   $('.undo').click(undo)
   $('.remove-item').click(removeItem);
+  $('tr:nth-child(3n)').each(function() {
+    umaPropaganda().insertAfter($(this))
+  });
 };
 $(aposInicializado);
